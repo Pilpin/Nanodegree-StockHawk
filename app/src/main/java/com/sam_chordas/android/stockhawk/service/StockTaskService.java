d1a6b34f495bc3aa753a4e391e7f366d5cff8155
pilpin <sylvain.autran@gmail.com>
2016-04-23 19:45:22 +0200

    The app doesn't crash when the user enters a non existing stock. Still need to program a way to let the user know.

--- app/src/main/java/com/sam_chordas/android/stockhawk/service/StockTaskService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/service/StockTaskService.java
@@ -110,6 +110,7 @@ public class StockTaskService extends GcmTaskService{
 
     if (urlStringBuilder != null){
       urlString = urlStringBuilder.toString();
+      Log.d(LOG_TAG, urlString);
       try{
         getResponse = fetchData(urlString);
         result = GcmNetworkManager.RESULT_SUCCESS;
@@ -121,8 +122,7 @@ public class StockTaskService extends GcmTaskService{
             mContext.getContentResolver().update(QuoteProvider.Quotes.CONTENT_URI, contentValues,
                 null, null);
           }
-          mContext.getContentResolver().applyBatch(QuoteProvider.AUTHORITY,
-              Utils.quoteJsonToContentVals(getResponse));
+          mContext.getContentResolver().applyBatch(QuoteProvider.AUTHORITY, Utils.quoteJsonToContentVals(getResponse));
         }catch (RemoteException | OperationApplicationException e){
           Log.e(LOG_TAG, "Error applying batch insert", e);
         }
